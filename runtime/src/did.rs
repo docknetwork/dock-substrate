86dd5a36fef8e20420db8214f58925a0162157df
lovesh <lovesh.bond@gmail.com>
2020-03-05 03:08:49 +0530

    add mock methods
    Signed-off-by: lovesh <lovesh.bond@gmail.com>

--- runtime/src/did.rs
+++ runtime/src/did.rs
@@ -44,6 +44,34 @@ impl Default for KeyDetail {
     }
 }
 
+/// This struct is passed as an argument while updating the key
+/// `cmd` is the command, in case of key update it will be 1.
+/// `did` is the DID whose key is being updated.
+/// `public_key_type` is new public key type
+/// `public_key` the new public key
+/// `controller` If provided None, the controller is unchanged. While serializing, use literal "None" when controller is None
+/// The last_modified_in_block is the block number when this DID was last modified is present to prevent replay attack
+#[derive(Encode, Decode, Clone, PartialEq, Debug)]
+pub struct KeyUpdate {
+    cmd: u8,
+    did: [u8; DID_BYTE_SIZE],
+    public_key_type: PublicKeyType,
+    public_key: Vec<u8>,
+    controller: Option<[u8; DID_BYTE_SIZE]>,
+    last_modified_in_block: u64
+}
+
+/// This struct is passed as an argument while removing the DID
+/// `cmd` is the command, in case of DID removal it is 2.
+/// `did` is the DID which is being removed.
+/// `last_modified_in_block` is the block number when this DID was last modified. The last modified time is present to prevent replay attack.
+#[derive(Encode, Decode, Clone, PartialEq, Debug)]
+pub struct DIDRemoval {
+    cmd: u8,
+    did: [u8; DID_BYTE_SIZE],
+    last_modified_in_block: u64,
+}
+
 decl_event!(
     pub enum Event<T>
     where
@@ -67,6 +95,8 @@ decl_module! {
         fn deposit_event() = default;
 
         //fn new(_origin, did: [u8; DID_BYTE_SIZE], detail: KeyDetail) -> DispatchResult {
+        /// Create a new DID.
+        /// `did` is the new DID to create. The method will throw exception if `did` is already registered.
         fn new(_origin, did: [u8; 32], detail: KeyDetail) -> DispatchResult {
             if Dids::<T>::exists(did) {
                 Self::deposit_event(RawEvent::DIDAlreadyExists(did.to_vec()));
@@ -78,5 +108,20 @@ decl_module! {
             Ok(())
         }
 
+        /// `signature` is the signature on the serialized `KeyUpdate`.
+        /// The node while processing this extrinsic, should create the above serialized `KeyUpdate`
+        /// using the stored data and try to verify the given signature with the stored key.
+        pub fn update_key(_origin, key_update: KeyUpdate, signature: Vec<u8>) -> DispatchResult {
+            // TODO:
+            Ok(())
+        }
+
+        /// `to_remove` contains the DID to be removed
+        /// `signature` is the signature on the serialized `DIDRemoval`.
+        /// The node while processing this extrinsic, should create the above serialized `DIDRemoval` using the stored data and try to verify the given signature with the stored key.
+        pub fn remove(_origin, to_remove: DIDRemoval, signature: Vec<u8>) -> DispatchResult {
+            // TODO:
+            Ok(())
+        }
     }
 }
