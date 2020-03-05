5144907803e08a3ad4d673fc3af6148b43fdabb4
Andrew Dirksen <andrew@dirksen.com>
2020-03-05 11:49:56 -0800

    cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -8,18 +8,18 @@ mod chain_spec;
 mod service;
 mod cli;
 
-pub use sc_cli::{VersionInfo, IntoExit, error};
+pub use sc_cli::{error, IntoExit, VersionInfo};
 
 fn main() -> Result<(), cli::error::Error> {
-	let version = VersionInfo {
-		name: "Substrate Node",
-		commit: env!("VERGEN_SHA_SHORT"),
-		version: env!("CARGO_PKG_VERSION"),
-		executable_name: "node-template",
-		author: "Anonymous",
-		description: "Template Node",
-		support_url: "support.anonymous.an",
-	};
+    let version = VersionInfo {
+        name: "Substrate Node",
+        commit: env!("VERGEN_SHA_SHORT"),
+        version: env!("CARGO_PKG_VERSION"),
+        executable_name: "node-template",
+        author: "Anonymous",
+        description: "Template Node",
+        support_url: "support.anonymous.an",
+    };
 
-	cli::run(std::env::args(), cli::Exit, version)
+    cli::run(std::env::args(), cli::Exit, version)
 }
