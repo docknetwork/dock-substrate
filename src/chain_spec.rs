91825b29fa66882d7137d5519207d0c58ad7a26d
Andrew Dirksen <andrew@dirksen.com>
2020-03-05 11:27:09 -0800

    Gets master compiling so we can start creating feature branches.

--- src/chain_spec.rs
+++ src/chain_spec.rs
@@ -1,5 +1,5 @@
 use sp_core::{Pair, Public, sr25519};
-use node_template_runtime::{
+use dock_testnet_runtime::{
 	AccountId, AuraConfig, BalancesConfig, GenesisConfig, GrandpaConfig,
 	SudoConfig, IndicesConfig, SystemConfig, WASM_BINARY, Signature
 };
