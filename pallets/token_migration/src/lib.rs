23c7bd5895a04b764d12ca40594e064f8a2ea592
lovesh <lovesh.bond@gmail.com>
2020-07-16 19:31:48 +0530

    Add token migration support
    Signed-off-by: lovesh <lovesh.bond@gmail.com>

--- pallets/token_migration/src/lib.rs
+++ pallets/token_migration/src/lib.rs
@@ -1,21 +1,23 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
+/// Pallet for token migration.
+
+use sp_std::marker::PhantomData;
 use codec::{Decode, Encode};
 use frame_support::{
-    decl_error, decl_event, decl_module, decl_storage, dispatch,
-    dispatch::IsSubType,
-    ensure, fail,
-    sp_runtime::{
-        traits::{CheckedSub, DispatchInfoOf, SaturatedConversion, SignedExtension},
-        transaction_validity::{
-            InvalidTransaction, TransactionValidity, TransactionValidityError, ValidTransaction,
-        },
+    decl_error, decl_event, decl_module, decl_storage, dispatch, ensure, fail, weights::Pays,
+    dispatch::{IsSubType},
+    debug::{RuntimeLogger, debug},
+    traits::{
+        Get, Currency, ExistenceRequirement::AllowDeath, WithdrawReason
     },
-    traits::{Currency, ExistenceRequirement::AllowDeath, Get, WithdrawReason},
-    weights::Pays,
+    sp_runtime::{print,
+                 traits::{SaturatedConversion, CheckedSub, SignedExtension, DispatchInfoOf},
+                 transaction_validity::{
+                     TransactionValidity, ValidTransaction, InvalidTransaction,
+                     TransactionValidityError,
+                 }}
 };
-/// Pallet for token migration.
-use sp_std::marker::PhantomData;
 
 use frame_system::{self as system, ensure_root, ensure_signed};
 extern crate alloc;
@@ -138,10 +140,7 @@ decl_module! {
 
 impl<T: Trait> Module<T> {
     /// Deduct tokens from the migrator's account
-    pub fn migrate_(
-        migrator: T::AccountId,
-        recipients: BTreeMap<T::AccountId, BalanceOf<T>>,
-    ) -> dispatch::DispatchResult {
+    pub fn migrate_(migrator:T::AccountId, recipients: BTreeMap<T::AccountId, BalanceOf<T>>) -> dispatch::DispatchResult {
         let allowed_migrations = Self::migrators(&migrator).unwrap();
         let mut mig_count = recipients.len() as u16;
         ensure!(
@@ -150,33 +149,17 @@ impl<T: Trait> Module<T> {
         );
 
         // The balance that needs to be transferred to all recipients combined
-        let total_transfer_balance = recipients
-            .values()
-            .fold(0u128, |acc, &x| {
-                acc.saturating_add(x.saturated_into::<u128>())
-            })
-            .saturated_into();
+        let total_transfer_balance = recipients.values().fold(0u128, |acc, &x| acc.saturating_add(x.saturated_into::<u128>())).saturated_into();
         // The balance of the migrator after the transfer
-        let new_free = T::Currency::free_balance(&migrator)
-            .checked_sub(&total_transfer_balance)
-            .ok_or(Error::<T>::InsufficientBalance)?;
+        let new_free = T::Currency::free_balance(&migrator).checked_sub(&total_transfer_balance).ok_or(Error::<T>::InsufficientBalance)?;
         // Ensure that the migrator can transfer, i.e. has sufficient free and unlocked balance
-        T::Currency::ensure_can_withdraw(
-            &migrator,
-            total_transfer_balance,
-            WithdrawReason::Transfer.into(),
-            new_free,
-        )?;
+        T::Currency::ensure_can_withdraw(&migrator, total_transfer_balance, WithdrawReason::Transfer.into(), new_free)?;
 
         for (recip, balance) in recipients {
             // There is a very slim change that transfer fails with an addition overflow when the recipient has a very high balance
             match T::Currency::transfer(&migrator, &recip, balance, AllowDeath) {
-                Ok(_) => Self::deposit_event(RawEvent::Migration(
-                    migrator.clone(),
-                    recip,
-                    balance.saturated_into(),
-                )),
-                Err(_) => mig_count -= 1,
+                Ok(_) => Self::deposit_event(RawEvent::Migration(migrator.clone(), recip, balance.saturated_into())),
+                Err(_) => mig_count -= 1
             }
         }
         Migrators::<T>::insert(migrator, allowed_migrations - mig_count);
@@ -196,8 +179,8 @@ impl<T: Trait + Send + Sync> sp_std::fmt::Debug for OnlyMigrator<T> {
 }
 
 impl<T: Trait + Send + Sync> SignedExtension for OnlyMigrator<T>
-where
-    <T as system::Trait>::Call: IsSubType<Module<T>, T>,
+    where
+        <T as system::Trait>::Call: IsSubType<Module<T>, T>,
 {
     const IDENTIFIER: &'static str = "OnlyMigrator";
     type AccountId = T::AccountId;
@@ -205,9 +188,7 @@ where
     type AdditionalSigned = ();
     type Pre = ();
 
-    fn additional_signed(&self) -> sp_std::result::Result<(), TransactionValidityError> {
-        Ok(())
-    }
+    fn additional_signed(&self) -> sp_std::result::Result<(), TransactionValidityError> { Ok(()) }
 
     fn validate(
         &self,
@@ -216,10 +197,11 @@ where
         _info: &DispatchInfoOf<Self::Call>,
         _len: usize,
     ) -> TransactionValidity {
+
         if let Some(local_call) = call.is_sub_type() {
             if let Call::migrate(..) = local_call {
                 if !<Migrators<T>>::contains_key(who) {
-                    return InvalidTransaction::Custom(1).into();
+                    return InvalidTransaction::Custom(1).into()
                 }
             }
         }
