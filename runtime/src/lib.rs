da870661f2bd8c620b31351ffd208a2fd2842d29
lovesh <lovesh.bond@gmail.com>
2020-09-11 23:19:37 +0530

    Add extrinsic execute_unchecked_weight
    Signed-off-by: lovesh <lovesh.bond@gmail.com>

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -130,7 +130,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("dock-testnet"),
     impl_name: create_runtime_str!("dock-runtime"),
     authoring_version: 1,
-    spec_version: 6,
+    spec_version: 7,
     impl_version: 1,
     transaction_version: 1,
     apis: RUNTIME_API_VERSIONS,
