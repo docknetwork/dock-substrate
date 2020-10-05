a308ea5e856799a1f292a0f79ec10bfc2832315b
lovesh <lovesh.bond@gmail.com>
2020-10-06 04:25:18 +0530

    Fix Master's weight computation and log event on failed execution
    Signed-off-by: lovesh <lovesh.bond@gmail.com>

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -129,7 +129,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     impl_name: create_runtime_str!("dock-main-runtime"),
     authoring_version: 1,
     spec_version: 10,
-    impl_version: 1,
+    impl_version: 2,
     transaction_version: 1,
     apis: RUNTIME_API_VERSIONS,
 };
