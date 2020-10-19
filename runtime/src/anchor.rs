1278ca20f193f102052c739dee1821990b4d51d9
Andrew Dirksen <andrew@dirksen.com>
2020-10-19 15:03:56 -0700

    module level doc for anchor.rs

--- runtime/src/anchor.rs
+++ runtime/src/anchor.rs
@@ -1,4 +1,8 @@
 //! Minimal proof of existence registry.
+//!
+//! Anchors are hashed once before being added to storage. To check whether an anchor exists
+//! query the "Anchors" map for the hash of the anchor. If a corresponding value exists, then the
+//! anchor exists and the value represents the block number when it was first published.
 
 use alloc::vec::Vec;
 use frame_support::{
