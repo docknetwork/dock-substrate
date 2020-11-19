c284889b76b3bff764a8b10d03770565a8ab27c7
Andrew Dirksen <andrew@dirksen.com>
2020-11-19 09:32:00 -0800

    pre-release housekeeping: bump spec version

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -129,7 +129,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("dock-main-runtime"),
     impl_name: create_runtime_str!("dock-main-runtime"),
     authoring_version: 1,
-    spec_version: 12,
+    spec_version: 13,
     impl_version: 1,
     transaction_version: 1,
     apis: RUNTIME_API_VERSIONS,
