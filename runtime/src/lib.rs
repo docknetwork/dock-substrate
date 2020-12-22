843f5157f2d8609a174ecf1d3b26c982e532ffc3
lovesh <lovesh.bond@gmail.com>
2020-12-23 02:12:51 +0530

    bump spec version
    Signed-off-by: lovesh <lovesh.bond@gmail.com>

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -137,7 +137,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("dock-main-runtime"),
     impl_name: create_runtime_str!("dock-main-runtime"),
     authoring_version: 1,
-    spec_version: 16,
+    spec_version: 17,
     impl_version: 1,
     transaction_version: 1,
     apis: RUNTIME_API_VERSIONS,
