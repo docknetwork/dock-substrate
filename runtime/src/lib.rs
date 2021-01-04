46d93963251fe127d3fe4f611e41f2d777ac2a83
Sam Hellawell <>
2021-01-04 20:15:48 +0000

    cooloff period

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -495,6 +495,7 @@ parameter_types! {
     pub const EnactmentPeriod: BlockNumber = 30;    // 30 sec
     pub const LaunchPeriod: BlockNumber = 3 * MINUTES;
     pub const VotingPeriod: BlockNumber = 2 * MINUTES;
+    pub const CooloffPeriod: BlockNumber = 1 * MINUTES;
     pub const FastTrackVotingPeriod: BlockNumber = 1 * MINUTES;
     /// 10K tokens
     pub const MinimumDeposit: Balance = 10_000 * DOCK;
