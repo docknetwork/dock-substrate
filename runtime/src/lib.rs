2908ce25f08852fba191b48be6f14f14b015c445
Andrew Dirksen <andrew@dirksen.com>
2021-04-20 12:36:47 -0700

    bump runtime spec_version

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -142,7 +142,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("dock-main-runtime"),
     impl_name: create_runtime_str!("dock-main-runtime"),
     authoring_version: 1,
-    spec_version: 19,
+    spec_version: 21,
     impl_version: 1,
     transaction_version: 1,
     apis: RUNTIME_API_VERSIONS,
