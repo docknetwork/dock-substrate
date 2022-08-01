fce2045a402d26383bf0183a5d5a4493dfc810e5
olegnn <olegnosov1@gmail.com>
2022-08-01 15:50:33 +0300

    Update version and deps

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -190,7 +190,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("dock-pos-dev-runtime"),
     impl_name: create_runtime_str!("Dock"),
     authoring_version: 1,
-    spec_version: 36,
+    spec_version: 37,
     impl_version: 1,
     transaction_version: 1,
     apis: RUNTIME_API_VERSIONS,
