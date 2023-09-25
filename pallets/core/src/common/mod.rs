671ff601605f0b0a4f8219e4441c8c58205fdcf7
olegnn <olegnosov1@gmail.com>
2023-09-25 15:54:31 +0200

    Introduce `CombineTypesAndLimits`

--- pallets/core/src/common/mod.rs
+++ pallets/core/src/common/mod.rs
@@ -1,3 +1,5 @@
+use core::marker::PhantomData;
+
 pub mod keys;
 pub mod limits;
 pub mod policy;
@@ -17,3 +19,42 @@ pub use types::*;
 /// All associated types and size limits for the encodable data structures used by the `dock-core`.
 pub trait TypesAndLimits: Types + Limits {}
 impl<T: Types + Limits> TypesAndLimits for T {}
+
+/// Combines two different types - first implementing `Types` and second implementing `Limits`
+/// to form a single type implementing both.
+#[derive(Clone, Eq, PartialEq, Default)]
+pub struct CombineTypesAndLimits<T: Types, L: Limits = ()>(PhantomData<(T, L)>);
+
+impl<T: Types, L: Limits> Types for CombineTypesAndLimits<T, L> {
+    type BlockNumber = T::BlockNumber;
+    type AccountId = T::AccountId;
+}
+
+impl<T: Types, L: Limits> Limits for CombineTypesAndLimits<T, L> {
+    type MaxAccumulatorLabelSize = L::MaxAccumulatorLabelSize;
+    type MaxAccumulatorParamsSize = L::MaxAccumulatorParamsSize;
+    type MaxAccumulatorPublicKeySize = L::MaxAccumulatorPublicKeySize;
+    type MaxAccumulatorAccumulatedSize = L::MaxAccumulatorAccumulatedSize;
+
+    type MaxDidDocRefSize = L::MaxDidDocRefSize;
+    type MaxDidServiceEndpointIdSize = L::MaxDidServiceEndpointIdSize;
+    type MaxDidServiceEndpointOrigins = L::MaxDidServiceEndpointOrigins;
+    type MaxDidServiceEndpointOriginSize = L::MaxDidServiceEndpointOriginSize;
+
+    type MaxStatusListCredentialSize = L::MaxStatusListCredentialSize;
+    type MinStatusListCredentialSize = L::MinStatusListCredentialSize;
+
+    type MaxIriSize = L::MaxIriSize;
+
+    type MaxBlobSize = L::MaxBlobSize;
+
+    type MaxOffchainParamsLabelSize = L::MaxOffchainParamsLabelSize;
+    type MaxOffchainParamsBytesSize = L::MaxOffchainParamsBytesSize;
+
+    type MaxBBSPublicKeySize = L::MaxBBSPublicKeySize;
+    type MaxBBSPlusPublicKeySize = L::MaxBBSPlusPublicKeySize;
+    type MaxPSPublicKeySize = L::MaxPSPublicKeySize;
+
+    type MaxMasterMembers = L::MaxMasterMembers;
+    type MaxPolicyControllers = L::MaxPolicyControllers;
+}
