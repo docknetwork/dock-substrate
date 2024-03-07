383ec538b108b1d11e590d205ab522b56476c9f8
olegnn <olegnosov1@gmail.com>
2024-03-07 15:01:54 +0400

    Bump up spec

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -203,7 +203,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("dock-pos-dev-runtime"),
     impl_name: create_runtime_str!("Dock"),
     authoring_version: 1,
-    spec_version: 53,
+    spec_version: 54,
     impl_version: 2,
     transaction_version: 2,
     apis: RUNTIME_API_VERSIONS,
