34ff1f5c60ad141eae0ff20c66dd595b20be764c
olegnn <olegnn@users.noreply.github.com>
2024-06-25 11:58:37 +0000

    deploy: 1cf578fdf4f2db15c97fb57462114c3634f2f126

---
+++ implementors/dock_core/modules/trust_registry/pallet/trait.Config.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"dock_runtime":[["impl <a class=\"trait\" href=\"dock_runtime/trust_registry/trait.Config.html\" title=\"trait dock_runtime::trust_registry::Config\">Config</a> for <a class=\"struct\" href=\"dock_runtime/struct.Runtime.html\" title=\"struct dock_runtime::Runtime\">Runtime</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ implementors/pallet_tips/pallet/trait.Config.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"dock_runtime":[["impl Config&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.unit.html\">()</a>&gt; for <a class=\"struct\" href=\"dock_runtime/struct.Runtime.html\" title=\"struct dock_runtime::Runtime\">Runtime</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
